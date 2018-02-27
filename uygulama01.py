c252f1cbc81159e53ebee4b8b665d18190d3ea4d
meryemozdogan <36603262+meryemozdogan@users.noreply.github.com>
2018-02-27 21:26:04 +0300

    Add files via upload

---
+++ uygulama01.py
@@ -0,0 +1,8 @@
+metin= """Kuğuların ölüm öncesi ezgileri şiirlerim,
+        Yalpalayan hayatımın kara çarşaflı
+        bekçi gizleri."""
+
+first_half= len(metin)
+print(first_half / 2)
+
+print(metin[:53])
