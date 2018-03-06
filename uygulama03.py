7fac797964cc29bd858efd45d253da25096945da
meryemozdogan <36603262+meryemozdogan@users.noreply.github.com>
2018-03-06 20:13:56 +0300

    Add files via upload

---
+++ uygulama03.py
@@ -0,0 +1,13 @@
+#Türkçe-Almanca rakamlar sözlüğü, 05.03.2018
+
+rakamlar= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(rakamlar)
+
+rakamlar_dict=  {"0": "Null", "1": "Eins", "2": "Zwei", "3": "Drei", "4": "Vier", "5": "Fünf", "6": "Sechs", "7": "Sieben", "8": "Acht", "9": "Neun"}
+
+print("5" in rakamlar_dict)
+
+print(rakamlar_dict.values())
+
+print(rakamlar_dict[input("Rakam yazınız:")])
